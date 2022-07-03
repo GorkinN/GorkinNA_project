ebec7748edc046cda5b35e067e5dff0716d113c5
Cahta911 <66055350+Cahta911@users.noreply.github.com>
2022-07-03 11:26:13 +0500

    Update ContentCard.js

--- project/src/component/ContentCard/ContentCard.js
+++ project/src/component/ContentCard/ContentCard.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import {GreenButton} from "../Button/Button";
 import prodPicDefault from './image/vegs.jpg';
 import {CardPictureContainer, CardPicture, CardSale, CardTitle, CardDescription, CardPrice, CardPriceOff} from "./ContentCardCommonJSX";
+import {RatingBox} from "../RatingBox/RatingBox";
+import starGoldenFilled from "../RatingBox/starGoldenFilled.svg";
+
 let Card = styled.li`
 position: relative;
 display:flex;
@@ -32,25 +35,44 @@ justify-content:center;
     margin-right:26px;
 }
 `;
-
+let RatingContainer=styled.div`
+position:absolute;
+top:0px;
+right:5px;
+padding:15px 15px;
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 18px;
+background: url('${starGoldenFilled}') no-repeat;
+background-position:50% 30%;
+background-size:110%;
+color: ${props => props.theme.secondaryColor};
+`;
 export const ContentCard = ({product}) => {
     const {
         salePercent : sale , 
         name : title = "Product Title",
         description = "Space for a small product description",
         priceUSD: price,
-        picture} = product; 
+        picture,
+        rating} = product; 
     let saleText = sale ? `-${(sale)}%` : "";
     let prevPrice = sale>0 ? Math.ceil(price/(1 - sale/100)*100)/100 : "";
     let priceText = price===0 ? `FREE` : `${price} USD`;
 
     return (
         <Card id={product.id}>
+            
             <CardPictureContainer>
                 <CardPicture src={`${picture || prodPicDefault}`} alt="product picture"/>
                 <CardSale>
                     {saleText}
                 </CardSale>
+                <RatingContainer>
+                    {rating}
+                </RatingContainer>
             </CardPictureContainer>
 
             <CardTitle>
