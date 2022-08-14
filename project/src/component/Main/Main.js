a6d3456d53a2b39d62ae7c335ca533505858f560
Cahta911 <66055350+Cahta911@users.noreply.github.com>
2022-08-14 19:59:47 +0500

    rearranged products list

--- project/src/component/Main/Main.js
+++ project/src/component/Main/Main.js
@@ -11,11 +11,7 @@ const Main = () => {
 
     //going to fetch productsGeneralObj
     let productsGeneralObj = [
-        {id:1,name:"Duck",description:"Tasty Duck",priceUSD:10.00,salePercent:10,measure:"kg",category:"Meat",rating:1,freshness:"Extra fresh",farm:"Gravity Falls",deliveryArea:"Europe",isBestSelling:true, picture:`./img/1.jpg`},
-        {id:2,name:"Beef",description:"Tasty Beef",priceUSD:20.00,salePercent:12,measure:"kg",category:"Meat",rating:2,freshness:"Rotten",farm:"Amitville",deliveryArea:"Brasil",isBestSelling:false,picture:`./img/2.webp`},
-        {id:3,name:"Pork",description:"Tasty Pork",priceUSD:7.60,salePercent:15,measure:"kg",category:"Meat",rating:3,freshness:"Fresh",farm:"Black Rock",deliveryArea:"Germany",isBestSelling:true,picture:`./img/3.jpg`},
-        {id:4,name:"Venison",description:"Tasty Venison",priceUSD:40.00,salePercent:17,measure:"kg",category:"Meat",rating:4,freshness:"Test youself",farm:"Elwyn forest",deliveryArea:"Asia",isBestSelling:false,picture:`./img/4.jpg`},
-        {id:5,name:"Chicken",description:"Tasty Chicken",priceUSD:5.23,salePercent:20,measure:"kg",category:"Meat",rating:5,freshness:"Extra fresh",farm:"Duskwood",deliveryArea:"Russia",isBestSelling:true,picture:`./img/5.jpg`},
+        
         {id:6,name:"Tomato",description:"Tasty Tomato",priceUSD:1.45,salePercent:21,measure:"kg",category:"Vegetables",rating:1,freshness:"Rotten",farm:"Albuquerque",deliveryArea:"Any",isBestSelling:false,picture:`./img/6.jpg`},
         {id:7,name:"Carrot",description:"Tasty Carrot",priceUSD:1.68,salePercent:22,measure:"kg",category:"Vegetables",rating:2,freshness:"Fresh",farm:"Antananarivu",deliveryArea:"Europe",isBestSelling:true,picture:`./img/7.jpg`},
         {id:8,name:"Cucumber",description:"Tasty Cucumber",priceUSD:2.32,salePercent:10,measure:"kg",category:"Vegetables",rating:3,freshness:"Test youself",farm:"Osaka",deliveryArea:"Brasil",isBestSelling:false,picture:`./img/8.png`},
@@ -38,7 +34,12 @@ const Main = () => {
         {id:25,name:"Brush",description:"Tasty Brush",priceUSD:0.30,salePercent:17,measure:"pcs",category:"Miscellaneous",rating:5,freshness:"",farm:"",deliveryArea:"Germany",isBestSelling:true,picture:`./img/25.jpg`},
         {id:26,name:"Pipe",description:"Tasty Pipe",priceUSD:3.95,salePercent:20,measure:"pcs",category:"Miscellaneous",rating:1,freshness:"",farm:"",deliveryArea:"Asia",isBestSelling:false,picture:`./img/26.jpg`},
         {id:27,name:"Table",description:"Tasty Table",priceUSD:150.00,salePercent:21,measure:"pcs",category:"Furniture",rating:2,freshness:"",farm:"",deliveryArea:"Russia",isBestSelling:true,picture:`./img/27.jpg`},
-        {id:28,name:"Chair",description:"Tasty Chair",priceUSD:50.00,salePercent:22,measure:"pcs",category:"Furniture",rating:3,freshness:"",farm:"",deliveryArea:"Any",isBestSelling:false,picture:`./img/28.jpg`}
+        {id:28,name:"Chair",description:"Tasty Chair",priceUSD:50.00,salePercent:22,measure:"pcs",category:"Furniture",rating:3,freshness:"",farm:"",deliveryArea:"Any",isBestSelling:false,picture:`./img/28.jpg`},
+        {id:1,name:"Duck",description:"Tasty Duck",priceUSD:10.00,salePercent:10,measure:"kg",category:"Meat",rating:1,freshness:"Extra fresh",farm:"Gravity Falls",deliveryArea:"Europe",isBestSelling:true, picture:`./img/1.jpg`},
+        {id:2,name:"Beef",description:"Tasty Beef",priceUSD:20.00,salePercent:12,measure:"kg",category:"Meat",rating:2,freshness:"Rotten",farm:"Amitville",deliveryArea:"Brasil",isBestSelling:false,picture:`./img/2.webp`},
+        {id:3,name:"Pork",description:"Tasty Pork",priceUSD:7.60,salePercent:15,measure:"kg",category:"Meat",rating:3,freshness:"Fresh",farm:"Black Rock",deliveryArea:"Germany",isBestSelling:true,picture:`./img/3.jpg`},
+        {id:4,name:"Venison",description:"Tasty Venison",priceUSD:40.00,salePercent:17,measure:"kg",category:"Meat",rating:4,freshness:"Test youself",farm:"Elwyn forest",deliveryArea:"Asia",isBestSelling:false,picture:`./img/4.jpg`},
+        {id:5,name:"Chicken",description:"Tasty Chicken",priceUSD:5.23,salePercent:20,measure:"kg",category:"Meat",rating:5,freshness:"Extra fresh",farm:"Duskwood",deliveryArea:"Russia",isBestSelling:true,picture:`./img/5.jpg`},
     ];
     
     let [isGridLayout, setLayout] = useState(true);
